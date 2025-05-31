5dfda83972e01ed2c39c26395483c2afa47c5c04
RattleT2 <mario.pratama2016@gmail.com>
2025-05-31 21:24:29 +0800

    update

---
+++ array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    std::string nama_list[] = {"Bayu", "Argus", "Santos", "Silver"};
+    int jumlah_nama = sizeof(nama_list) / sizeof(nama_list[0]); 
+    
+    for (int i = 0; i < jumlah_nama; i++) {
+        std::cout << nama_list[i] << std::endl;
+    }
+    return 0;
+}
